882f0f0556eebc4d79c0b7e322589a5f77cf7605
Nick Cain <nicholascain@google.com>
2022-12-08 23:39:33 +0000

    Update agent and webhook components with usability changes

--- dialogflow-cx/vpc-sc-demo/components/webhook/deploy_webhook.sh
+++ dialogflow-cx/vpc-sc-demo/components/webhook/deploy_webhook.sh
@@ -26,6 +26,7 @@ export PREFIX="terraform/${PROJECT_ID?}/webhook"
 # Initialize:
 gcloud --quiet auth login "${PRINCIPAL?}" --no-launch-browser
 gcloud config set project "${PROJECT_ID?}"
+gcloud services enable cloudresourcemanager.googleapis.com
 
 
 sudo docker run -w /app -v "$(pwd)":/app "${TERRAFORM_IMAGE?}" init -reconfigure -backend-config="access_token=$(gcloud auth print-access-token)" -backend-config="bucket=${TF_PLAN_STORAGE_BUCKET?}" -backend-config="prefix=${PREFIX?}"
