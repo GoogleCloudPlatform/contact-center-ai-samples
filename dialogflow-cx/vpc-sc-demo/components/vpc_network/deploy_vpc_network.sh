5b099e2b0c8020fa419573e9cf8f2de109a598ec
Nick Cain <nicholascain@google.com>
2022-12-12 22:29:08 +0000

    Add deploy_service_directory script

--- dialogflow-cx/vpc-sc-demo/components/vpc_network/deploy_vpc_network.sh
+++ dialogflow-cx/vpc-sc-demo/components/vpc_network/deploy_vpc_network.sh
@@ -29,6 +29,6 @@ gcloud --quiet auth login "${PRINCIPAL?}" --no-launch-browser
 gcloud config set project "${PROJECT_ID?}"
 gcloud services enable cloudresourcemanager.googleapis.com
 
-sudo docker run -w /app -v "$(pwd)":/app "${TERRAFORM_IMAGE?}" init -reconfigure -backend-config="access_token=$(gcloud auth print-access-token)" -backend-config="bucket=${TF_PLAN_STORAGE_BUCKET?}" -backend-config="prefix=${PREFIX?}"
+sudo docker run -w /app -v "$(pwd)":/app "${TERRAFORM_IMAGE?}" init -upgrade -reconfigure -backend-config="access_token=$(gcloud auth print-access-token)" -backend-config="bucket=${TF_PLAN_STORAGE_BUCKET?}" -backend-config="prefix=${PREFIX?}"
 
 sudo docker run -w /app -v "$(pwd)":/app -e GOOGLE_OAUTH_ACCESS_TOKEN="$(gcloud auth print-access-token)" "${TERRAFORM_IMAGE?}" apply --auto-approve -var project_id="${PROJECT_ID?}"
