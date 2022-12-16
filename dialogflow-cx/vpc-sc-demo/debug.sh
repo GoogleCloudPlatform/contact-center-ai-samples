48f846a65293d7e54e88a6c4c6d08cbc16357bb4
Nick Cain <nicholascain@google.com>
2022-12-16 03:09:28 +0000

    reorganize container folder structure

--- dialogflow-cx/vpc-sc-demo/debug.sh
+++ dialogflow-cx/vpc-sc-demo/debug.sh
@@ -21,4 +21,14 @@ export USER_SERVICE_TAG='dev'
 sudo docker build --build-arg PROD=false -t ${USER_SERVICE_IMAGE?}:${USER_SERVICE_TAG_BASE?} .
 sudo docker build -t ${USER_SERVICE_IMAGE?}:${USER_SERVICE_TAG?} -f Dockerfile.dev .
 
-sudo docker run -it -p 5001:5001 -p 3001:3001 --rm --entrypoint=/app/debug_runner.sh -v "$(pwd)"/backend:/app -v "$(pwd)"/frontend:/frontend vpc-sc-demo:dev
+sudo docker run -it \
+  -p 5001:5001 \
+  -p 3001:3001 \
+  --rm \
+  --entrypoint=/backend/debug_runner.sh \
+  -v "$(pwd)"/backend:/backend \
+  -v "$(pwd)"/frontend:/frontend \
+  -v "$(pwd)"/deploy:/deploy \
+  -v "$(pwd)"/components/webhook/telecom-webhook-src:/components/telecom-webhook-src \
+  -v "$(pwd)"/components/reverse_proxy_server/proxy-server-src:/components/proxy-server-src \
+  vpc-sc-demo:dev
