71bc091b526200cfff7f2d36dafd3688c0aacffe
Kristopher Overholt <koverholt@google.com>
2023-08-14 15:46:37 -0500

    Optimize linters and tests (#481)
    * Empty commit to test linting
    
    * Add Terraform linter
    
    * Remove super linter
    
    * Update Lint Terraform action
    
    * Update TF linter
    
    * Add test app
    
    * Update tflint
    
    * Disable some long tests
    
    * Update Terraform version constraints for providers to use new format
    
    * Add required_version to Terraform files
    
    * Fix Terraform dependency name
    
    * Remove test file
    
    * Add Python linter
    
    * Update Python linter
    
    * Update Python linter
    
    * Fix Python linter
    
    * Fix Python linting
    
    * Update Python linter
    
    * Adjust Python linter options
    
    * Update imports
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * isort
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    * Update eslint
    
    * Update linters
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Add Markdown linter
    
    * Update
    
    * Update
    
    * Fix Markdown files
    
    * Add YAML linter
    
    * Update
    
    * Update
    
    * Update
    
    * Update YAML files
    
    * Update
    
    * Fix YAML files
    
    * Change eslint linter
    
    * Try another eslint action
    
    * Remove eslint
    
    ---------
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

--- dialogflow-cx/vpc-sc-demo/components/service_directory/main.tf
+++ dialogflow-cx/vpc-sc-demo/components/service_directory/main.tf
@@ -18,14 +18,21 @@ variable "project_id" {
 }
 
 terraform {
-  required_providers {
-    google      = "~> 4.77.0"
-    google-beta = "~> 4.77.0"
+    required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.77.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 4.77.0"
+    }
   }
   backend "gcs" {
     bucket = null
     prefix = null
   }
+  required_version = ">= 1.2.0"
 }
 
 variable "region" {
